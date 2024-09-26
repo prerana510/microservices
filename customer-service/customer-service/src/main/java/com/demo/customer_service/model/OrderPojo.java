7499fce2908535385e7cb46a1afccc928ed32056
Prerana Joshi <preranajoshi510@gmail.com>
2024-09-26 05:27:05 -0700

    Add files via upload

---
+++ customer-service/customer-service/src/main/java/com/demo/customer_service/model/OrderPojo.java
@@ -0,0 +1,15 @@
+package com.demo.customer_service.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderPojo {
+    private Long orderId;
+    private String orderName;
+    private Long custId;
+}
